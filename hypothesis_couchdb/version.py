dd9cda497a336aca89386ed8372f9cb8a03a27c6
Alexander Shorin <kxepal@gmail.com>
2016-02-01 21:17:24 +0300

    Fix setup tools warning about version format

--- hypothesis_couchdb/version.py
+++ hypothesis_couchdb/version.py
@@ -17,8 +17,8 @@
 __version_info__ = (0, 1, 0, 'dev', 0)
 __version__ = '%(version)s%(tag)s%(build)s' % {
     'version': '.'.join(map(str, __version_info__[:3])),
-    'tag': '-' + __version_info__[3] if __version_info__[3] else '',
-    'build': '.' + str(__version_info__[4]) if __version_info__[4] else ''
+    'tag': '.' + __version_info__[3] if __version_info__[3] else '',
+    'build': '' + str(__version_info__[4]) if __version_info__[3] else ''
 }
 
 
